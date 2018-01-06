4c18a9990c838d8c0c76d098c89f485f4a18448c
YonePC <YonePC@YONE>
2018-01-06 07:42:59 +0000

    character component

--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CharactersRoutingModule } from './characters-routing.module';
 import { CharactersComponent } from './characters.component';
+import { CharacterComponent } from './character.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CharactersRoutingModule
   ],
-  declarations: [CharactersComponent]
+  declarations: [CharactersComponent, CharacterComponent]
 })
 export class CharactersModule { }
