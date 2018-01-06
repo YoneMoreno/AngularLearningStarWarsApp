0df594de4caddcdeedff83a53f098c11bc74c560
YonePC <YonePC@YONE>
2018-01-06 09:00:17 +0000

    core module

--- core/core.module.ts
+++ core/core.module.ts
@@ -1,14 +1,20 @@
-import {RouterModule} from '@angular/router';
-import {FormsModule} from '@angular/forms';
-import {CommonModule} from '@angular/common';
 import {NgModule, Optional, SkipSelf} from '@angular/core';
-import {ExceptionService} from './exception.service';
-import {EntityService} from './entity.service';
-import {MessageService} from './message.service';
-import {NavComponent} from '../src/app/core/nav/nav.component';
-import {ToastModule} from '../src/app/core/toast/toast.module';
-import {ModalModule} from "../src/app/core/modal/modal.module";
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
+import {EntityService} from './entity.service';
+import {ExceptionService} from './exception.service';
+import {MessageService} from './message.service';
+import {ModalModule} from '../src/app/core/modal/modal.module';
+import {SpinnerModule} from '../src/app/core/spinner/spinner.module';
+import {ToastModule} from '../src/app/core/toast/toast.module';
+import {NavComponent} from '../src/app/core/nav/nav.component';
+
+// imports: imports the module's exports. which is usually declarables and providers
+// in our case the spinner has no providers.
+//
+// exports: exports modules AND components/directives/pipes that other modules may want to use
 @NgModule({
   imports: [
     CommonModule, FormsModule, RouterModule,
@@ -30,4 +36,5 @@ export class CoreModule {
     throwIfAlreadyLoaded(parentModule, 'CoreModule');
   }
 }
-})
+
+
