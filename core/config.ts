2201e9df89074214dea760c9b6b6e05bb5a1dae0
YonePC <YonePC@YONE>
2018-01-06 08:26:41 +0000

    config file

---
+++ core/config.ts
@@ -0,0 +1,8 @@
+export let CONFIG = {
+  baseUrls: {
+    config: 'commands/config',
+    resetDb: 'commands/resetDb',
+    characters: 'api/character.json',
+    vehicles: 'api/vehicles.json'
+  }
+};
