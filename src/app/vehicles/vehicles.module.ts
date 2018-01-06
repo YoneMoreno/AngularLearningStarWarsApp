e430340d11664a8d881cb47e039247bfa52a4c77
YonePC <YonePC@YONE>
2018-01-06 07:52:47 +0000

    vehicle-list c

--- src/app/vehicles/vehicles.module.ts
+++ src/app/vehicles/vehicles.module.ts
@@ -4,12 +4,13 @@ import { CommonModule } from '@angular/common';
 import { VehiclesRoutingModule } from './vehicles-routing.module';
 import { VehiclesComponent } from './vehicles.component';
 import { VehicleComponent } from './vehicle/vehicle.component';
+import { VehicleListComponent } from './vehicle-list/vehicle-list.component';
 
 @NgModule({
   imports: [
     CommonModule,
     VehiclesRoutingModule
   ],
-  declarations: [VehiclesComponent, VehicleComponent]
+  declarations: [VehiclesComponent, VehicleComponent, VehicleListComponent]
 })
 export class VehiclesModule { }
