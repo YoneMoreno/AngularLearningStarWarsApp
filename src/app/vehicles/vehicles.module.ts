3f1e2bb55bc157cffebb87021c64b44249789878
YonePC <YonePC@YONE>
2018-01-06 07:49:15 +0000

    vehicles m

---
+++ src/app/vehicles/vehicles.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { VehiclesRoutingModule } from './vehicles-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    VehiclesRoutingModule
+  ],
+  declarations: []
+})
+export class VehiclesModule { }
