58ddb8422291534b4b80dadaeaecf4e0d8af3191
YonePC <YonePC@YONE>
2018-01-06 07:45:36 +0000

    character-button c

--- src/app/characters/characters.module.ts
+++ src/app/characters/characters.module.ts
@@ -5,12 +5,13 @@ import { CharactersRoutingModule } from './characters-routing.module';
 import { CharactersComponent } from './characters.component';
 import { CharacterComponent } from './character.component';
 import { CharacterListComponent } from './character-list.component';
+import { CharacterButtonComponent } from './shared/character-button.component';
 
 @NgModule({
   imports: [
     CommonModule,
     CharactersRoutingModule
   ],
-  declarations: [CharactersComponent, CharacterComponent, CharacterListComponent]
+  declarations: [CharactersComponent, CharacterComponent, CharacterListComponent, CharacterButtonComponent]
 })
 export class CharactersModule { }
