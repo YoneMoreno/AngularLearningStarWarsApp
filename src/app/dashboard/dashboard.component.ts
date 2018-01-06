e61bf1620684c64c6dd86dde4518d2cc327b6ef8
YonePC <YonePC@YONE>
2018-01-06 09:32:28 +0000

    dashboard component

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,26 +1,60 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Observable} from 'rxjs/Observable';
 import {Character} from '../../../models/character.model';
 import {Subscription} from 'rxjs/Subscription';
 import {ActivatedRoute, Router} from '@angular/router';
 import {CharacterService} from '../../../models/character.service';
+import {ToastService} from '../core/toast/toast.service';
+import 'rxjs/add/operator/do';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, OnDestroy {
+
   private dbResetSubscription: Subscription;
 
   characters: Observable<Character[]>;
   title: string;
 
   constructor(private route: ActivatedRoute,
-              private characterService: CharacterService) {
+              private characterService: CharacterService,
+              private router: Router,
+              private toastService: ToastService) {
+  }
+
+  getCharacters() {
+    this.characters = this.characterService.getCharacters()
+      .do(() => this.toastService.activate('Got characters for the dashboard'))
+      .catch(e => {
+        this.toastService.activate(`${e}`);
+        return Observable.of([]);
+      });
+  }
+
+  gotoDetail(character: Character) {
+    const link = ['/characters', character.id];
+    this.router.navigate(link);
+  }
+
+
+  ngOnDestroy() {
+    this.dbResetSubscription.unsubscribe();
   }
 
   ngOnInit() {
+    this.route.data.subscribe((data: { title: string }) => {
+      this.title = data.title;
+    });
+    this.getCharacters();
+    this.dbResetSubscription = this.characterService.onDbReset.subscribe(() =>
+      this.getCharacters()
+    );
   }
 
+  trackByCharacters(index: number, character: Character) {
+    return character.id;
+  }
 }
