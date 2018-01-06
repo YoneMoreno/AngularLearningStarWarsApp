a8e5040dc842f6d00bf7d11e7f3ca0bf7be1c730
YonePC <YonePC@YONE>
2018-01-06 07:50:33 +0000

    vehicles c

---
+++ src/app/vehicles/vehicles.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicles',
+  template: `
+    <p>
+      vehicles works!
+    </p>
+  `,
+  styles: []
+})
+export class VehiclesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/vehicles/vehicles.module.ts
+++ src/app/vehicles/vehicles.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { VehiclesRoutingModule } from './vehicles-routing.module';
+import { VehiclesComponent } from './vehicles.component';
 
 @NgModule({
   imports: [
     CommonModule,
     VehiclesRoutingModule
   ],
-  declarations: []
+  declarations: [VehiclesComponent]
 })
 export class VehiclesModule { }
