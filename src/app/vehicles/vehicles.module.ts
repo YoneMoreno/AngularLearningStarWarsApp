1790a7842a26ab430abb8ab5e4dc4312eb937631
YonePC <YonePC@YONE>
2018-01-06 07:51:39 +0000

    vehicle c

--- src/app/vehicles/vehicles.module.ts
+++ src/app/vehicles/vehicles.module.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { VehiclesRoutingModule } from './vehicles-routing.module';
 import { VehiclesComponent } from './vehicles.component';
+import { VehicleComponent } from './vehicle/vehicle.component';
 
 @NgModule({
   imports: [
     CommonModule,
     VehiclesRoutingModule
   ],
-  declarations: [VehiclesComponent]
+  declarations: [VehiclesComponent, VehicleComponent]
 })
 export class VehiclesModule { }
